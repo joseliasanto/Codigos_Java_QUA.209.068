c834f65e201d8d4adb37e690ffa11e73f12c294b
joselias952@gmail.com <joselias952@gmail.com>
2023-08-02 17:46:52 -0300

    primeira aula de logica e programação

---
+++ OlaMundo/src/Principal/OlaMundo.java
@@ -0,0 +1,19 @@
+package Principal;
+
+classe principal
+public class OlaMundo {
+
+	//metodo principal, onde executa o programa
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; ln = new line;
+		System.out.println("Hello word,Olá Mundo");
+
+	//comentario de umalnha
+		/*
+		 * esse é
+		 * um comentario
+		 * de multiplas linhas
+		 */
+
---
+++ variaveis/src/principal/variaveis.java
@@ -0,0 +1,45 @@
+package principal;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int numero;
+    numero =10;
+    double decimal =10.50;
+    //aspas duplas para strings
+    String nome = "Joselia";
+    //aspas simples para char
+    char sexo ='M';
+    //booleanos = valores verdadeiros ou falsos
+    boolean verdadeiro = true;
+    boolean falso = false;
+    
+    int testeNumero =(int)50.25;
+    double testeNovoNumero = 10;
+    int idade = 40;
+    double peso = 79.0;
+    	
+    System.out.println(numero);
+    System.out.println(decimal);
+    System.out.println(testeNumero);
+    System.out.println(testeNovoNumero);	
+    System.out.println(sexo);
+    System.out.println(nome);
+    
+    System.out.println(verdadeiro);
+    System.out.println(falso);
+  System.out.print("Nome:");
+    System.out.println(" Joselia");
+    //para organizar o codigo usamos o
+    //ctrl + shift + f
+    System.out.println("***********");
+    System.out.print("Meu nome é: " + nome);
+    System.out.println("Minha idade é:" + idade);
+    System.out.println("Meu peso é:" + peso);
+    System.out.print(",tenho" + idade);
+    
+    
+    
+	}   
+}
