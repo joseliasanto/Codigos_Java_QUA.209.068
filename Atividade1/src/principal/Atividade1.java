2069e84945159a500eff99c5770567995d7342a2
Joselia <joselias952@gmail.com>
2023-08-03 14:51:48 -0300

    Atividade1 java

---
+++ Atividade1/src/principal/Atividade1.java
@@ -0,0 +1,34 @@
+package principal;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		int numero1 = 1;
+		int numero2 = 2;
+		
+	System.out.println(numero1);
+	System.out.println(numero2);
+	
+	String nome = "Joselia Santos";
+	
+	char sexo = 'F';
+
+	System.out.println("Joselia Sntos");
+	System.out.println(sexo);
+	
+	boolean verdadeiro = true;
+	
+	boolean falso = false;
+	
+	System.out.println(verdadeiro);
+	System.out.println(falso);
+	
+	String nome1 = "Joselia ";
+	String nome2 = "Santos";
+	System.out.println(nome1 + nome2);
+	
+		
+
+	}
+
+}
