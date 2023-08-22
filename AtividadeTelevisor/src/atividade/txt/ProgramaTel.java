7b154a02bfb377290d0ce537350a4bdfe76868b6
Joselia <joselias952@gmail.com>
2023-08-22 17:34:46 -0300

    Atividades Encapsulamento,Herança e

--- AtividadeTelevisor/src/atividade/txt/ProgramaTel.java
+++ AtividadeTelevisor/src/atividade/txt/ProgramaTel.java
@@ -1,5 +1,25 @@
 package atividade.txt;
 
 public class ProgramaTel {
+	
+	
+	public static void main(String[] args) {
+		
+		Televisor tv = new Televisor();
+		
+		tv.obterStatus();
+		tv.ligarTelevisor();
+		tv.obterStatus();
+		
+		
+		
+		
+		
+		
+		
+		}
+	
+	
 
 }
+
